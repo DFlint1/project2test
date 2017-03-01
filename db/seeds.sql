5311f2d65ec186f7edae1ba8c269d50b4fa49d39
Dana Flint <Dana_Flint@msn.com>
2017-03-01 16:29:51 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO burgers(burger_name) VALUES ('Vince Young');
+INSERT INTO burgers(burger_name) VALUES ('Colt McCoy');
+INSERT INTO burgers(burger_name) VALUES ('Major Applewhite');
+INSERT INTO burgers(burger_name) VALUES ('Chris Simms');
+INSERT INTO burgers(burger_name) VALUES ('Garrett Gilbert');
+INSERT INTO burgers(burger_name) VALUES ('James Brown');javascript
+
+
+-- INSERT INTO `burgeryummy_db`.`users` (`id`, `name`, `password`, `email`, `userType`, `createdAt`, `updatedAt`) VALUES ('1', 'Tom', '123', 'tom@gmail.com', 'lawyer', '11:11:11', '11:11:11');
+-- INSERT INTO `burgeryummy_db`.`users` (`id`, `name`, `password`, `email`, `userType`, `createdAt`, `updatedAt`) VALUES ('2', 'Bill', '321', 'bill@yahoo.com', 'user', '11:11:11', '11:11:11');
+-- INSERT INTO `burgeryummy_db`.`users` (`id`, `name`, `password`, `email`, `userType`, `createdAt`, `updatedAt`) VALUES ('3', 'George', '123', 'george@gmailcom', 'user', '11:11:11', '11:11:11');
+-- INSERT INTO `burgeryummy_db`.`users` (`id`, `name`, `password`, `email`, `userType`, `createdAt`, `updatedAt`) VALUES ('4', 'Sally', '321', 'sally@gmailcom', 'laywer', '11:11:11', '11:11:11');
+-- INSERT INTO `burgeryummy_db`.`users` (`id`, `name`, `password`, `email`, `userType`, `createdAt`, `updatedAt`) VALUES ('5', 'Jane', '123', 'jane@bing.com', 'user', '11:11:11', '11:11:11');
+-- INSERT INTO `burgeryummy_db`.`users` (`id`, `name`, `password`, `email`, `userType`, `createdAt`, `updatedAt`) VALUES ('6', 'Sue', '321', 'sue@hotmail.com', 'user', '11:11:11', '11:11:11');
+
+
+
